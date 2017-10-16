f715fbc72f816e534cf80323cea58cddc2bfce83
NajmaZahra <najmazahra792@gmail.com>
2017-10-16 20:25:24 +0700

    abc

---
+++ app/src/main/java/genius/mykhatamulquranbeta/startOfflineActivity.java
@@ -0,0 +1,13 @@
+package genius.mykhatamulquranbeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class startOfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_offline);
+    }
+}
