60faa75c0753ebfd523b83c70e667e6edd07ce55
DIJAHPROGRAMING <32635964+papabego123@users.noreply.github.com>
2017-10-16 16:52:01 +0700

    KUY LAH

---
+++ app/src/main/java/genius/mykhatamulquranbeta/MainActivityRegister.java
@@ -0,0 +1,13 @@
+package genius.mykhatamulquranbeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_register);
+    }
+}
