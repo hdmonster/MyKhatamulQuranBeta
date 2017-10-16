0f91f89fe6868e8baa453659d2f9e5b573239be1
Deathgun <32635968+koalapanda@users.noreply.github.com>
2017-10-17 06:23:49 +0700

    yang sudah di intent

--- app/src/main/java/genius/mykhatamulquranbeta/FullscreenActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/FullscreenActivity.java
@@ -1,18 +1,20 @@
 package genius.mykhatamulquranbeta;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class    FullscreenActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -93,8 +95,6 @@ public class FullscreenActivity extends AppCompatActivity {
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
-
-        // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,10 +102,14 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        Button reg = (Button) findViewById(R.id.button2);
+        reg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                Intent reg = new Intent(FullscreenActivity.this,MainActivityRegister.class);
+                startActivity(reg);
+            }
+        });
     }
 
     @Override
