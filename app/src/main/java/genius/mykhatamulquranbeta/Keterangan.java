a38495820cf7e478c6942753b02d80bdd818737a
hdmonster <heydardzakis@gmail.com>
2017-10-17 10:41:41 +0700

    dzakyy

--- app/src/main/java/genius/mykhatamulquranbeta/Keterangan.java
+++ app/src/main/java/genius/mykhatamulquranbeta/Keterangan.java
@@ -1,7 +1,10 @@
 package genius.mykhatamulquranbeta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Keterangan extends Activity {
 
@@ -9,5 +12,14 @@ public class Keterangan extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keterangan);
+
+        Button mulai = (Button) findViewById(R.id.btn_mulai);
+        mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Keterangan.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/genius/mykhatamulquranbeta/LoginActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
@@ -14,6 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        TextView off = (TextView) findViewById(R.id.txtOff);
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(LoginActivity.this, startOfflineActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
