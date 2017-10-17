35ccae652ab1d1c1cee7fcf4049943ca9a2bfe38
hdmonster <heydardzakis@gmail.com>
2017-10-17 10:40:01 +0700

    Linked 50%

--- app/src/main/java/genius/mykhatamulquranbeta/startOfflineActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/startOfflineActivity.java
@@ -1,7 +1,10 @@
 package genius.mykhatamulquranbeta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class startOfflineActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class startOfflineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_offline);
+
+        Button online = (Button) findViewById(R.id.btn_online);
+        online.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(startOfflineActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
