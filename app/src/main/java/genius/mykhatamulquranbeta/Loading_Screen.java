5aee660d4643e6f3203965dcceaa1a3f0a15a428
NajmaZahra <najmazahra792@gmail.com>
2017-10-17 10:39:15 +0700

    najmazahra

--- app/src/main/java/genius/mykhatamulquranbeta/Loading_Screen.java
+++ app/src/main/java/genius/mykhatamulquranbeta/Loading_Screen.java
@@ -3,12 +3,17 @@ package genius.mykhatamulquranbeta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Loading_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_loading__screen);
     Thread myThread = new Thread(){
        @Override
