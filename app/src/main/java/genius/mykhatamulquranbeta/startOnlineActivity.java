7815a615ad28a85b05464fc7b97d170fc0c434ea
hdmonster <heydardzakis@gmail.com>
2017-10-18 11:45:11 +0700

    Fixed Record List

--- app/src/main/java/genius/mykhatamulquranbeta/startOnlineActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/startOnlineActivity.java
@@ -13,6 +13,15 @@ public class startOnlineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_online);
 
+        Button rec = (Button) findViewById(R.id.btn_record);
+        rec.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(startOnlineActivity.this, RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
         /*Button graphics = (Button) findViewById(R.id.btn_grafik);
         graphics.setOnClickListener(new View.OnClickListener() {
             @Override
