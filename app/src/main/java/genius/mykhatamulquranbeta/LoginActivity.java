b7edd544ba4f8bc79aa71ab0a429466baecc6a7f
NajmaZahra <najmazahra792@gmail.com>
2017-10-18 12:05:25 +0700

    70

--- app/src/main/java/genius/mykhatamulquranbeta/LoginActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login_activity);
 
         TextView reg = (TextView) findViewById(R.id.txtReg);
--- app/src/main/java/genius/mykhatamulquranbeta/startOnlineActivity.java
+++ app/src/main/java/genius/mykhatamulquranbeta/startOnlineActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class startOnlineActivity extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class startOnlineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_online);
 
 
