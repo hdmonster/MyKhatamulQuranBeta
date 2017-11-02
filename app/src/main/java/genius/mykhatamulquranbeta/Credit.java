69975f00878890c700a8e22a8eea032c51a1f478
DIJAHPROGRAMING <32635964+papabego123@users.noreply.github.com>
2017-11-03 06:44:44 +0700

    miji

---
+++ app/src/main/java/genius/mykhatamulquranbeta/Credit.java
@@ -0,0 +1,9 @@
+package genius.mykhatamulquranbeta;
+
+/**
+ * Created by ALIF_PC on 11/1/2017.
+ */
+
+public class Credit extends MainActivity {
+
+}
--- app/src/main/java/genius/mykhatamulquranbeta/util/Credit.java
+++
@@ -1,8 +0,0 @@
-package genius.mykhatamulquranbeta.util;
-
-/**
- * Created by ALIF_PC on 11/1/2017.
- */
-
-public class Credit {
-}
