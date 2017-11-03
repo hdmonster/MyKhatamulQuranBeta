e328eb0ce24d12aa3100fc70af21d213ee427b70
DIJAHPROGRAMING <32635964+papabego123@users.noreply.github.com>
2017-11-03 07:34:52 +0700

    miji

--- app/src/main/java/genius/mykhatamulquranbeta/Credit.java
+++ app/src/main/java/genius/mykhatamulquranbeta/Credit.java
@@ -1,9 +1,21 @@
 package genius.mykhatamulquranbeta;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
 /**
  * Created by ALIF_PC on 11/1/2017.
  */
 
 public class Credit extends MainActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.credit);
+    }
 }
---
+++ app/src/main/java/genius/mykhatamulquranbeta/tutorial.java
@@ -0,0 +1,35 @@
+package genius.mykhatamulquranbeta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by ALIF_PC on 11/3/2017.
+ */
+
+public class tutorial extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.tutorial);
+
+        Button sls = (Button) findViewById(R.id.btn_sls);
+        sls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(tutorial.this, startOnlineActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
