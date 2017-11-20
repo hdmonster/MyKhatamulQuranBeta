0e9903f645add60aad21a771084dd1c095b90300
Deathgun <32635968+koalapanda@users.noreply.github.com>
2017-11-20 11:54:22 +0700

    fix tutorial & profil

---
+++ app/src/main/java/genius/mykhatamulquranbeta/profil/daffaprofile.java
@@ -0,0 +1,34 @@
+package genius.mykhatamulquranbeta.profil;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import genius.mykhatamulquranbeta.R;
+
+/**
+ * Created by DAFFA_PC on 19/11/2017.
+ */
+
+public class daffaprofile extends Fragment {
+    ImageView pg1;
+    Context context;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_daffa, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        context = getActivity();
+        pg1 = (ImageView) getView().findViewById(R.id.imageView6);
+        pg1.setImageResource(R.mipmap.profile1);
+    }
+}
